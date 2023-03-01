d22a00acf2483d937ae25acf16d23a6e223f44fd
Shivacharan2001 <126329853+Shivacharan2001@users.noreply.github.com>
2023-03-01 17:09:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const App=()=>{
+    return(
+        <>
+        <div>
+        <div>App</div>
+        <h1>hello</h1>
+        </div>
+        <p>bye</p>
+        </>
+    )
+}
+
+export default App
